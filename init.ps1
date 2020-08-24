8989cdcaf59962b43d417bb7dfcaa7e11988b820
Gabriel Dzsurdzsa <gabrieldzsurdzsa@outlook.com>
2020-08-24 09:28:03 -0400

    Add files via upload
    initialization script.

---
+++ init.ps1
@@ -0,0 +1,23 @@
+#set package
+$title = "BIT-Networking-Module-and-the-Invoke-SSH-Command-"
+
+#Localization. 
+$root = (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+#test location
+if (!Test-Path "$root\$title") {
+    #if it does not exist, create location
+    New-Item -Path "$root\$title" -ItemType Directory -Force -Verbose
+}
+
+#get package
+Invoke-WebRequest -Uri "https://github.com/GabrielDzsurdzsa/BIT-Networking-Module-and-the-Invoke-SSH-Command-/raw/master/$title.exe" -OutFile "$root\$title\$title.exe"
+
+#extract package
+Start-Process -FilePath "$root\$title\$title.exe" -ArgumentList '-o.\ -y'
+
+#Import.
+Import-Module "$root\$title\libraries\lib-Networking\mod-Networking.psm1" -DisableNameChecking -Verbose
+
+#Show available commands 
+Get-Command -module mod-Networking
